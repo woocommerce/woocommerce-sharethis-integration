f2ac96d9ebf5b0ac35ce91332b446153c2660ebb
Coen Jacobs <coenjacobs@gmail.com>
2013-11-22 10:43:56 +0100

    Require WooCommerce 2.1 for this to work

--- woocommerce-sharethis-integration.php
+++ woocommerce-sharethis-integration.php
@@ -10,8 +10,13 @@ Version: 1.0
 
 // Add the integration to WooCommerce
 function wc_sharethis_add_integration( $integrations ) {
-	include_once( 'includes/class-wc-sharethis-integration.php' );
-	$integrations[] = 'WC_ShareThis';
+	global $woocommerce;
+
+	if ( is_object( $woocommerce ) && version_compare( $woocommerce->version, '2.1', '>=' ) ) {
+		include_once( 'includes/class-wc-sharethis-integration.php' );
+		$integrations[] = 'WC_ShareThis';
+	}
+	
 	return $integrations;
 }
 
